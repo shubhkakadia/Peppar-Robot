fb16eebdfa271b5315fa6f3ddf32ac97df67b21e
Shubh Kakadia <58301611+shubhkakadia@users.noreply.github.com>
2023-05-01 19:44:53 +0930

    accessing pepper using JS

--- html/js/feedback.js
+++ html/js/feedback.js
@@ -1,134 +1,104 @@
-// let selectedValue = 0;
-let fetchData = null;
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-session= null;
-// QiSession(connected, disconnected, location.host);
+const utilityFunction = function (callback) {
+  console.log(RobotUtils);
+  RobotUtils.onServices(function (ALLeds, ALTextToSpeech) {
+    ALLeds.randomEyes(2.0);
+    ALTextToSpeech.say("I will take over the world");
+  });
+};
 
-// function connected(s){
-//   console.log("session connected");
-//   session = s;
-// }
+utilityFunction();
 
-// function disconnected(error){
-//   console.log("session disconnected")
-//   session = null;
-// }
-
-const submitBtn = document.getElementById("submitBtn")
-
-// submitBtn.addEventListener('click', function () {
-//   session.service("ALTextToSpeech").then(function(tts){
-//     tts.say("hello working.");
-//   });
-//   console.log("Clicked");
-//   window.location.href="/pages/menu.html"
-
-// });
-
-function setValue(RatingValue) {
-
-
-  // session.service("ALTextToSpeech").then(function(tts){
-  //   tts.say("hello working.");
-  // });
-  console.log("Clicked");
-  window.location.href="menu.html";
-  // window.location.href="/pages/menu.html"
-
-  const sele = document.getElementById("sele");
-  // const Name = document.getElementById("Name").value;
-  // const ServiceType = document.getElementById("ServiceType").value;
-  // const Comments = document.getElementById("Comment").value;
-
-  sele.innerHTML = "changed";
-  // validation: if service type is empty dont submit the request.
-  // if (ServiceType !== "") {
-  //   async function FetchAPI() {
-  //     var requestOptions = {
-  //       method: "GET",
-  //       redirect: "follow",
-  //     };
-
-  //     await fetch("http://192.168.0.115:3000/feedback/get", requestOptions)
-  //       .then((response) => response.text())
-  //       .then((result) => {
-  //         fetchData = JSON.parse(result).response;
-  //       })
-  //       .catch((error) => console.log("error", error));
-  //   }
-
-  //   function generateID() {
-  //     console.log(fetchData);
-  //     if (fetchData.length < 1) {
-  //       ID = (currentYear - 2000) * 10000;
-  //       console.log(ID);
-  //       return ID + 1;
-  //     } else {
-  //       return fetchData.at(-1).ID + 1;
-  //     }
-  //   }
-
-  //   function generateJSON() {
-  //     const ID = generateID();
-  //     const Conversation = [];
-
-  //     let feedbackData = {
-  //       ID,
-  //       Name,
-  //       ServiceType,
-  //       Comments,
-  //       Rating,
-  //       Conversation,
-  //     };
-
-  //     console.log("feedbackData", feedbackData);
-  //     return feedbackData;
-  //   }
-
-  //   function post_req() {
-  //     const raw = JSON.stringify(generateJSON());
-  //     console.log(raw);
-  //     var myHeaders = new Headers();
-  //     myHeaders.append("Content-Type", "application/json");
-
-  //     var requestOptions = {
-  //       method: "POST",
-  //       headers: myHeaders,
-  //       body: raw,
-  //       redirect: "follow",
-  //     };
-
-  //     fetch("http://192.168.0.115:3000/feedback/register", requestOptions)
-  //       .then((response) => response.text())
-  //       .then((result) => console.log(result))
-  //       .catch((error) => console.log("error", error));
-  //   }
-
-  //   await FetchAPI();
-  //   Rating = ratingvalue;
-  //   post_req();
-
-    // const customAlert = document.createElement("div");
-    // customAlert.classList.add("custom-alert");
-    // customAlert.innerHTML = `
-    // <div class="alert alert-success d-flex align-items-center" role="alert">
-    //   <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-    //   <div>
-    //     Thank you for your feedback! Redirecting you to the Menu.
-    //   </div>
-    // </div>
-    // `;
-    // document.body.appendChild(customAlert);
-    // // Set custom alert position to top of page
-    // customAlert.style.position = "fixed";
-    // customAlert.style.top = "0";
-    // customAlert.style.left = "34.5%";
-    // customAlert.style.opacity = "80%";
-
-    // // After 5 seconds, redirect to menu.html
-    // setTimeout(() => {
-    //   window.location.href ="menu.html";
-    // },3000);
+var fetchData = null;
+var currentDate = new Date();
+var currentYear = currentDate.getYear() + 1900;
+function FetchAPI() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://192.168.0.115:3000/feedback/get", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      fetchData = JSON.parse(xhr.responseText).response;
+      console.log(fetchData);
+      // fetchData = fetchData.response;
+    }
+  };
+  xhr.send(null);
+}
+FetchAPI();
+function setValue(ratingvalue) {
+  // switch(ratingvalue){
+  //   case 1:
   // }
+
+  var Name = document.getElementById("Name").value;
+  var ServiceType = document.getElementById("ServiceType").value;
+  var Comments = document.getElementById("Comment").value;
+  if (ServiceType !== "") {
+    function generateID() {
+      console.log(fetchData);
+      if (fetchData.length < 1) {
+        ID = (currentYear - 2000) * 10000;
+        console.log(ID);
+        return ID + 1;
+      } else {
+        return fetchData[fetchData.length - 1].ID + 1;
+      }
+    }
+    function generateJSON() {
+      var ID = generateID();
+      var Conversation = [];
+      var feedbackData = {
+        ID: ID,
+        Name: Name,
+        ServiceType: ServiceType,
+        Comments: Comments,
+        Rating: ratingvalue,
+        Conversation: Conversation,
+      };
+      console.log("feedbackData", feedbackData);
+      return feedbackData;
+    }
+    function post_req() {
+      var xhr = new XMLHttpRequest();
+      var url = "http://192.168.0.115:3000/feedback/register";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          console.log(xhr.responseText);
+        }
+      };
+      var data = JSON.stringify(generateJSON());
+      xhr.send(data);
+    }
+    FetchAPI();
+    post_req();
+    var customAlert = document.createElement("div");
+    customAlert.className = "custom-alert";
+    customAlert.innerHTML =
+      '<div class="alert alert-success d-flex align-items-center" role="alert">' +
+      '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>' +
+      "<div>" +
+      "Thank you for your feedback! Redirecting you to the Menu." +
+      "</div>" +
+      "</div>";
+    document.body.appendChild(customAlert);
+    // Set custom alert position to top of page
+    customAlert.style.position = "fixed";
+    customAlert.style.top = "0";
+    customAlert.style.left = "34.5%";
+    customAlert.style.opacity = "80%";
+
+    // +var tts = new ALProxy("ALTextToSpeech", "192.168.0.160", 9559); // replace "your-pepper-robot-ip-address" with the IP address of your Pepper robot
+
+    // session.say("Thank you for your feedback");
+
+    // sayThankYou();
+    RobotUtils.onServices(function (ALLeds, ALTextToSpeech) {
+      ALLeds.randomEyes(2.0);
+      ALTextToSpeech.say("hello got the message");
+    });
+    setTimeout(function () {
+      window.location.href = "menu.html";
+    }, 3000);
+  }
 }
