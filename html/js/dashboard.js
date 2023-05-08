11ca8582aff268641fc5c839b6a8673fbc9e31dd
John Rallos <jrallos418@gmail.com>
2023-05-08 18:59:24 +0930

    updated (Logout) not working
    goToHome() ALTabletService

--- html/js/dashboard.js
+++ html/js/dashboard.js
@@ -3,6 +3,7 @@
 
 const dashboard = document.getElementById("dashboard");
 
+
 dashboard.addEventListener('click', function () {
     console.log("Clicked");
     location.href = 'pages/menu.html'
@@ -17,11 +18,10 @@ function logout() {
 	if (username === "admin" && password === "admin") {
 		// Clear any user session data or cookies
 		// Redirect the user to the logout page or homepage
-        RobotUtils.onServices(function (ALMemory, ALTabletService) {
-            ALMemory.raiseEvent("ClosedAllApps", "");
-            console.log(ALMemory);
-            ALTabletService.logout();
-            console.log(ALTabletService);
+		
+        RobotUtils.onServices(function (ALTabletService) {
+			ALTabletService.goToHome();
+			console.log(ALTabletService);
           });
 
 	} else {
