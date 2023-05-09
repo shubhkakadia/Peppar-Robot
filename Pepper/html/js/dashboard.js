4df2b5c1d96f0950e1e42e51e334752a0ffb4f7b
John Rallos <jrallos418@gmail.com>
2023-05-09 11:12:37 +0930

    FIxed logout
    Logout button now exits the application of Pepper.

--- Pepper/html/js/dashboard.js
+++ Pepper/html/js/dashboard.js
@@ -1,5 +1,5 @@
-// session = null
-// QiSession(connected, disconnected, location.host)
+session = null
+QiSession(connected, disconnected, location.host)
 
 const dashboard = document.getElementById("dashboard");
 
@@ -9,20 +9,52 @@ dashboard.addEventListener('click', function () {
     location.href = 'pages/menu.html'
 });
 
+
+function connected(s) {
+    console.log("Session connected");
+    session = s;
+    //If you want to subscribe so some events (to send info pepper->tablet) call the function here
+}
+
+function disconnected(error) {
+    console.log("Session disconnected");
+}
+
 function logout() {
+	
 	// Get the admin username and password from the user
-	var username = prompt("Enter admin username:");
 	var password = prompt("Enter admin password:");
 
-	// Verify the admin credentials before logging out
-	if (username === "admin" && password === "admin") {
+	// // Verify the admin credentials before logging out
+	if (password === "admin") {
+		session.service("ALMemory").then(function (memory) {
+			memory.raiseEvent("clickedExit", true);
+		});
 		// Clear any user session data or cookies
-		// Redirect the user to the logout page or homepage
+
+		// RobotUtils.onServices(function (ALBehaviorManager) {
+		// 	session.service("ALBehaviorManager").then(function (ALBehaviorManager) {
+		// 		ALBehaviorManager.stopAllBehaviors();
+		// 		console.log(ALBehaviorManager);
+
+		// 		session.service("ALSystem").then(function (ALSystem) {
+		// 			ALSystem.exit();
+		// 		});
+		// 	});
+		// });
 		
-        RobotUtils.onServices(function (ALTabletService) {
-			ALTabletService.goToHome();
-			console.log(ALTabletService);
-          });
+		// RobotUtils.subscribeToALMemoryEvent("clickedExit", true);
+
+		
+
+        // RobotUtils.onServices(function (ALMemory, ALBehaviorManager) {
+		// 	console.log(ALMemory)
+		// 	ALMemory.raiseEvent("clickedExit", true)
+		// 	// ALBehaviorManager.stopAllBehaviors()
+		// 	// console.log(ALBehaviorManager);
+		// 	// ALSystem.exit();
+		// 	// console.log(ALSystem);
+        //   });
 
 	} else {
 		alert("Incorrect admin username or password!");
