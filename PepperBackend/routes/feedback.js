9fa9ecf6ede8d1ee20b1c51ae3ff1f66afe3472b
John Rallos <jrallos418@gmail.com>
2023-07-04 23:44:19 +0930

    Added the remove route
    Added the remove route for the feedback-related components. The remove function is associated with the DELETE /delete/:ID route, where :ID is a parameter representing the ID of the feedback entry to be deleted.

--- PepperBackend/routes/feedback.js
+++ PepperBackend/routes/feedback.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router(); // Use Router instead of the app instance
-const { register, read } = require("../controller/feedback");
+const { register, read, remove } = require("../controller/feedback");
 
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
@@ -8,5 +8,6 @@ router.use(bodyParser.json());
 // Define the routes
 router.post("/register", register); // Handle POST requests to /register
 router.get("/get", read); // Handle GET requests to /get
+router.delete("/delete/:ID", remove); // Handle DELETE requests to /delete/:ID
 
 module.exports = router;
